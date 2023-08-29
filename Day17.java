011e486d89cd586d49be5c9c03f047bbf2925b23
Chandrika Dhale <89838366+CHANDRIKA5687@users.noreply.github.com>
2023-08-29 08:31:57 +0530

    Add files via upload

---
+++ Day17.java
@@ -0,0 +1,17 @@
+package ninjaslayground;
+
+//linear Serarch
+
+import java.util.*;
+public class Day17 {
+    public static int linearSearch(int n, int num, int []arr){
+        // Write your code here.
+        for(int i=0;i<n;i++){
+            if(arr[i]==num){
+                return i;
+            }
+
+        }
+        return -1;
+    }
+}
